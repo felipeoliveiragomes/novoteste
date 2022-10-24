5b22b2084b3dff1259517f11373213127a7efd8a
felipeoliveiragomes <91478220+felipeoliveiragomes@users.noreply.github.com>
2022-10-24 15:49:41 -0300

    Add files via upload
    Adcionando script

---
+++ ScriptFirewall.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+iptables -P INPUT DROP
+
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT   
+
+iptables -A INPUT -m state --state NEW -p tcp --dport 80 -j ACCEPT
+
+iptables -A INPUT -m state --state NEW -p tcp --dport 443 -j ACCEPT
+
+iptables -A INPUT -s  $IP -j ACCEPT
+
+iptables -A  INPUT -m mac --mac-source $MAC -j ACCEPT 
+
+iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
+
+iptables -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
+
+
+iptables -F INPUT
