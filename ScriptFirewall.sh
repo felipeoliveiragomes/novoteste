0d0adf252f6187b08f9ecd6961eb55cbb6307da0
felipeoliveiragomes <91478220+felipeoliveiragomes@users.noreply.github.com>
2022-10-24 22:02:38 -0300

    Fazendo um update tomara que de certo

--- ScriptFirewall.sh
+++ ScriptFirewall.sh
@@ -3,26 +3,41 @@
 iptables -P INPUT DROP
 
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT   
+iptables -A INPUT -p tcp --sport 22 -j ACCEPT   
 
 iptables -A INPUT -m state --state NEW -p tcp --dport 80 -j ACCEPT
+iptables -A OUTPUT -m state --state NEW -p tcp --sport 80 -j ACCEPT
 
 iptables -A INPUT -m state --state NEW -p tcp --dport 443 -j ACCEPT
+iptables -A OUTPUT -m state --state NEW -p tcp --sport 443 -j ACCEPT
 
 iptables -A INPUT -s  $IP -j ACCEPT
+iptables -A INPUT -d  $IP -j ACCEPT
 
-iptables -A  INPUT -m mac --mac-source $MAC -j ACCEPT 
 
 iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
+iptables -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
 
 iptables -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
+iptables -A OUTPUT -p icmp --icmp-type echo-reply -j ACCEPT
 
 iptables -A INPUT -p tcp --dport 21 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 20 -j ACCEPT
 
-iptables -A INPUT -p tcp --dport 25 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+
+iptables -A INPUT -p tcp --dport 25  -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 25  -j ACCEPT
 
 
-iptables -A INPUT -p tcp --dport 143 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+
+iptables -A INPUT -p tcp --dport 143 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 143  -j ACCEPT
 
 
 
 iptables -F INPUT
+
+
+#https://www.digitalocean.com/community/tutorials/iptables-essentials-common-firewall-rules-and-commands
+#https://serverfault.com/questions/38398/allowing-ftp-with-iptables
+#https://www.cyberciti.biz/tips/linux-iptables-9-allow-icmp-ping.html
