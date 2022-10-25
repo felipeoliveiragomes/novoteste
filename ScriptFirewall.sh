8b38b3cafa4547742cbe9c60cb500324db489c34
felipeoliveiragomes <91478220+felipeoliveiragomes@users.noreply.github.com>
2022-10-24 22:18:56 -0300

    nova mudanca, tomara que de certo

--- ScriptFirewall.sh
+++ ScriptFirewall.sh
@@ -2,34 +2,43 @@
 
 iptables -P INPUT DROP
 
+# Para liberar o SSH
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT   
 iptables -A INPUT -p tcp --sport 22 -j ACCEPT   
 
+
+#Para Liberar HTTP
 iptables -A INPUT -m state --state NEW -p tcp --dport 80 -j ACCEPT
 iptables -A OUTPUT -m state --state NEW -p tcp --sport 80 -j ACCEPT
 
+#Para liberar o HTTPS
 iptables -A INPUT -m state --state NEW -p tcp --dport 443 -j ACCEPT
 iptables -A OUTPUT -m state --state NEW -p tcp --sport 443 -j ACCEPT
 
-iptables -A INPUT -s  $IP -j ACCEPT
-iptables -A INPUT -d  $IP -j ACCEPT
 
+# Permite para uma maquina especifica
+iptables -A INPUT -s  $IP -j ACCEPT
+iptables -A OUTPUT -d  $IP -j ACCEPT
+
+#para liberar a requisiscao de icmp
 
 iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 iptables -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
 
+#Para liberar a resposta de icmp
 iptables -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
 iptables -A OUTPUT -p icmp --icmp-type echo-reply -j ACCEPT
 
+#para liberar FTP
 iptables -A INPUT -p tcp --dport 21 -j ACCEPT
 iptables -A OUTPUT -p tcp --sport 20 -j ACCEPT
 
-
+#Para liberar o SMTP
 iptables -A INPUT -p tcp --dport 25  -j ACCEPT
 iptables -A OUTPUT -p tcp --sport 25  -j ACCEPT
 
 
-
+#Para liberar o IMAP
 iptables -A INPUT -p tcp --dport 143 -j ACCEPT
 iptables -A OUTPUT -p tcp --sport 143  -j ACCEPT
 
@@ -41,3 +50,4 @@ iptables -F INPUT
 #https://www.digitalocean.com/community/tutorials/iptables-essentials-common-firewall-rules-and-commands
 #https://serverfault.com/questions/38398/allowing-ftp-with-iptables
 #https://www.cyberciti.biz/tips/linux-iptables-9-allow-icmp-ping.html
+#https://serverfault.com/questions/163111/allow-traffic-to-from-specific-ip-with-iptables
